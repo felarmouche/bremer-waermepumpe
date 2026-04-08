de6ad0fc18ef991424d5e86b4347326b99f43c64
ferri <ferris@uni-bremen.de>
2026-04-08 07:39:15 +0200

    mvp 90%

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bremer-waermepumpe.de',
+
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
   },
